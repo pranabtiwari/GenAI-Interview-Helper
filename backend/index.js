3133bf7631fe689375c38c95da91c80280737915
pranabtiwari <trpanav006@gmail.com>
2026-04-22 11:32:32 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import user from '../routes/user.routes.js';
-import page from '../routes/page.routes.js';
-import interView from '../routes/interview.routes.js';
+import user from './routes/user.routes.js';
+import page from './routes/page.routes.js';
+import interView from './routes/interview.routes.js';
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 import cors from 'cors';
+
+dotenv.config();
+connectDB();
+
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const publicDir = path.resolve(__dirname, "../public");
+const publicDir = path.resolve(__dirname, "./public");
 
 
 // Middleware
@@ -30,7 +36,11 @@ app.use('/api', interView);
 
 app.use(express.static(publicDir));
 
-app.get((req, res, next) => {
+app.get("/", (req, res) => {
+    return res.sendFile(path.join(publicDir, "index.html"));
+});
+
+app.get("/{*path}", (req, res, next) => {
     if (req.originalUrl.startsWith("/api")) {
         return next();
     }
@@ -38,4 +48,9 @@ app.get((req, res, next) => {
     return res.sendFile(path.join(publicDir, "index.html"));
 });
 
-export default app;
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`running ${port}`);
+});
--- backend/services/ai.services.js
+++ backend/services/ai.services.js
@@ -1,5 +1,8 @@
 import Groq from "groq-sdk";
 import puppeteer from "puppeteer-core";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
